07e311002f70e2e9cc17b660ea3a0bf1cb0e949b
Anderson <anderalw@gmail.com>
2020-04-20 22:21:59 -0300

    desafio

--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,8 +1,39 @@
+import fs from 'fs';
+import neatCsv from 'neat-csv';
+
+import CreateTransactionService from './CreateTransactionService';
+
 import Transaction from '../models/Transaction';
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
+
 class ImportTransactionsService {
-  async execute(): Promise<Transaction[]> {
-    // TODO
+  async execute(importFile: string): Promise<Transaction[]> {
+    const createTransaction = new CreateTransactionService();
+
+    const rawData = await fs.promises.readFile(importFile);
+
+    const data = await neatCsv<CreateTransactionDTO>(rawData, {
+      mapHeaders: ({ header }) => header.trim(),
+      mapValues: ({ value }) => value.trim(),
+    });
+
+    const newTransactions: Transaction[] = [];
+
+    for (const row of data) {
+      const transaction = await createTransaction.execute(row);
+
+      newTransactions.push(transaction);
+    }
+
+    await fs.promises.unlink(importFile);
+
+    return newTransactions;
   }
 }
 
